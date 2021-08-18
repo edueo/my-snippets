76c71417d410678e92a5710510f72954acc49751
Eduardo Alencar <ealencar10@gmail.com>
2021-08-18 15:51:53 -0300

    Create setup_logging.py

---
+++ python/setup_logging.py
@@ -0,0 +1,3 @@
+import logging
+logging.basicConfig(filename='log_filename.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('NÃO VOU MAIS USAR PRINT PARA DEBUGAR!!!')
